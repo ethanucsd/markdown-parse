8842c07fdad35d4c3c6e1029160c1e4b6ef614aa
Nyapaw <45115274+Nyapaw@users.noreply.github.com>
2022-01-27 16:56:26 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,11 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MarkdownParseTest {
@@ -12,24 +15,26 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testNoEntries() throws IOException {
-        Path fileName = Path.of("noEntries");
+    public void testNoEntries() throws IOException, URISyntaxException {
+        Path fileName = Path.of("noEntries.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(0, links.size());
     }
     
     @Test
-    public void testLinkWithParenthesis() throws IOException {
-        Path fileName = Path.of("linkWithParenthesis");
+    public void testLinkWithParenthesis() throws IOException, URISyntaxException {
+        URL url = this.getClass().getResource("linkWithParenthesis.md");
+        Path fileName = Paths.get(url.toURI());
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals("https://something.com", links.get(0));
     }
     
     @Test
-    public void testBrokenRef() throws IOException {
-        Path fileName = Path.of("brokenRef");
+    public void testBrokenRef() throws IOException, URISyntaxException {
+        URL url = this.getClass().getResource("brokenRef.md");
+        Path fileName = Paths.get(url.toURI());
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(0, links.size());
